49cb9e4dffde5ee3053cceefdf19fe4753ce20cd
ChinthakayalaShruthi <shruthichinthakayala@gmail.com>
2022-11-09 22:14:51 -0600

    update

--- controllers/ballon.js
+++ controllers/ballon.js
@@ -36,3 +36,37 @@ exports.ballon_list = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 }; 
+
+// VIEWS 
+// Handle a show all view 
+exports.ballon_view_all_Page = async function(req, res) { 
+    try{ 
+        theBallons = await Ballon.find(); 
+        res.render('ballon', { title: 'Ballon Search Results', results: theBallons }); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
+
+// Handle Costume create on POST. 
+exports.ballon_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new Ballon(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    document.gas = req.body.gas; 
+    document.colour = req.body.colour; 
+    document.count = req.body.count; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
--- routes/ballon.js
+++ routes/ballon.js
@@ -1,7 +1,15 @@
-var express = require('express');
-var router = express.Router();
+//var express = require('express');
+//var router = express.Router();
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('ballon', { title: 'Search Results' });
-});
+//router.get('/', function(req, res, next) {
+//  res.render('ballon', { title: 'Search Results' });
+//});
 module.exports = router;
+
+var express = require('express'); 
+const ballon_controlers= require('../controllers/ballon'); 
+var router = express.Router(); 
+ 
+/* GET costumes */ 
+router.get('/', ballon_controlers.ballon_view_all_Page ); 
+module.exports = router; 
