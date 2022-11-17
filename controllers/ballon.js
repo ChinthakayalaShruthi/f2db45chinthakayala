4b2cecf6f1b48c4244346e6b146a93cb682c8a77
ChinthakayalaShruthi <shruthichinthakayala@gmail.com>
2022-11-16 18:47:19 -0600

    updated ballon

--- controllers/ballon.js
+++ controllers/ballon.js
@@ -154,3 +154,17 @@ exports.ballon_update_Page =  async function(req, res) {
         res.send(`{'error': '${err}'}`); 
     } 
 };
+
+// Handle a delete one view with id from query 
+exports.ballon_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Ballon.findById(req.query.id) 
+        res.render('ballondelete', { title: 'Ballon Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
