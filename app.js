264985138b93f359fbc221d0e4352a9ac0ecd47c
ChinthakayalaShruthi <shruthichinthakayala@gmail.com>
2022-11-17 19:49:13 -0600

    update

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
+ 
+ 
 
 
 require('dotenv').config(); 
