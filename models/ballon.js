b0c25c8a54d8d54f975f63e873e06b96526bbe53
ChinthakayalaShruthi <shruthichinthakayala@gmail.com>
2022-11-30 01:57:23 -0600

    updated

--- models/ballon.js
+++ models/ballon.js
@@ -2,8 +2,11 @@ const mongoose = require("mongoose")
 const ballonSchema = mongoose.Schema({ 
  gas: String, 
  colour: String,
- count: Number
-}) 
+ count:{
+    type:Number,
+    min: 10,
+    max: 30 }
+ }) 
  
 module.exports = mongoose.model("Ballon", 
 ballonSchema)
