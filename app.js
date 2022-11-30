73320615670825928260c1b15aee7f87b5adfd0b
ChinthakayalaShruthi <shruthichinthakayala@gmail.com>
2022-11-30 01:42:27 -0600

    updated

--- app.js
+++ app.js
@@ -3,29 +3,30 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var passport = require('passport'); 
-var LocalStrategy = require('passport-local').Strategy; 
- 
- 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 
-require('dotenv').config(); 
-const connectionString =  
-process.env.MONGO_CON 
-mongoose = require('mongoose'); 
-mongoose.connect(connectionString,  
-{useNewUrlParser: true, 
-useUnifiedTopology: true});
+require('dotenv').config();
+const connectionString =
+  process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
 //Get the default connection 
-var db = mongoose.connection; 
- 
-//Bind connection to error event  
-db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
-db.once("open", function(){ 
-  console.log("Connection to DB succeeded")}); 
+var db = mongoose.connection;
 
-  var Ballon = require("./models/ballon");
+//Bind connection to error event  
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
+
+var Ballon = require("./models/ballon");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -44,6 +45,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -52,13 +60,24 @@ app.use('/ballon', ballonRouter);
 app.use('/gridbuild', gridbuildRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
+
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
+ 
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -68,38 +87,43 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
-
-
 // We can seed the collection if needed on server start 
-async function recreateDB(){ 
+async function recreateDB() {
   // Delete everything 
-  await Ballon.deleteMany(); 
- 
-  let instance1 = new 
-Ballon({gas:"nitrogen",  colour:'purple', 
-count:11}); 
-  instance1.save( function(err,doc) { 
-      if(err) return console.error(err); 
-      console.log("First object saved") 
-  }); 
+  await Ballon.deleteMany();
 
-  let instance2 = new 
-  Ballon({gas:"oxygen",  colour:'blue', 
-  count:14}); 
-    instance2.save( function(err,doc) { 
-        if(err) return console.error(err); 
-        console.log("First object saved") 
-    }); 
+  let instance1 = new
+    Ballon({
+      gas: "nitrogen", colour: 'purple',
+      count: 11
+    });
+  instance1.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved")
+  });
 
-    let instance3 = new 
-    Ballon({gas:"carbon dioxide",  colour:'green', 
-    count:22}); 
-      instance3.save( function(err,doc) { 
-          if(err) return console.error(err); 
-          console.log("First object saved") 
-      }); 
-} 
- 
-let reseed = true; 
-if (reseed) { recreateDB();}
+  let instance2 = new
+    Ballon({
+      gas: "oxygen", colour: 'blue',
+      count: 14
+    });
+  instance2.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved")
+  });
+
+  let instance3 = new
+    Ballon({
+      gas: "carbon dioxide", colour: 'green',
+      count: 22
+    });
+  instance3.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved")
+  });
+}
+
+let reseed = true;
+if (reseed) { recreateDB(); }
+
+module.exports = app;
--- routes/ballon.js
+++ routes/ballon.js
@@ -10,6 +10,15 @@ var express = require('express');
 const ballon_controlers= require('../controllers/ballon'); 
 var router = express.Router(); 
  
+// A little function to check if we have an authorized user and continue on or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 /* GET ballon */ 
 router.get('/', ballon_controlers.ballon_view_all_Page ); 
 module.exports = router; 
@@ -20,8 +29,8 @@ router.get('/detail', ballon_controlers.ballon_view_one_Page);
 /* GET create ballon page */ 
 router.get('/create', ballon_controlers.ballon_create_Page); 
 
-/* GET create update page */ 
-router.get('/update', ballon_controlers.ballon_update_Page); 
+/* GET update ballon page */ 
+router.get('/update', secured, ballon_controlers.ballon_update_Page); 
 
 /* GET delete ballon page */ 
 router.get('/delete', ballon_controlers.ballon_delete_Page); 
